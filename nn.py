90ce2739fdf9461093a8f4c5810c11e8eafc3104
khussey45 <khussey45@gmail.com>
2023-03-28 12:46:11 -0400

    g

--- nn.py
+++ nn.py
@@ -7,7 +7,12 @@ data = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
-class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt','Sneaker', 'Bag', 'Ankle boot']
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt','Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+print(train_images[7])
 
 plt.imshow(train_images[7], cmap=plt.cm.binary)
 plt.show()
