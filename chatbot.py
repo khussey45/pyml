06c1fef8429c233f993aa1b107dbb657f9c6e645
khussey45 <khussey45@gmail.com>
2023-04-06 13:38:57 -0400

    d

--- chatbot.py
+++ chatbot.py
@@ -93,7 +93,29 @@ def bag_of_words(s, words):
     for se in s_words:
         for i, w in enumerate(words):
             if w == se:
-                bag[i].append(1)
+                bag[i] = 1
     
     return numpy.array(bag)
+
+def chat():
+    print("Start talking with the bot! (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+        
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag  = labels[results_index]
+
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
+        else: 
+            print("I didn't get that, try again.")
             
+chat()
+
